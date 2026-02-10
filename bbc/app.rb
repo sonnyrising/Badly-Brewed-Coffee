fb2ad2d2ce0385e2ac5e7bfb8d87217907d1b7d9
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-02-10 18:00:32 +0000

    Fixed app.rb for new file structure.

--- bbc/app.rb
+++ bbc/app.rb
@@ -1,9 +1,11 @@
 require "require_all"
 require "sinatra"
 
-require_all "controllers"
+require_all File.join(__dir__, 'controllers')
+#require_all "../controllers"
 require_relative "helpers/helpers"
 require_relative "db/db"
-require_all "models"
+#require_all "../models"
+require_all File.join(__dir__, 'models')
 
 enable :sessions
