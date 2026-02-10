c1f750737ae6ec0e4cd7e522dd6d3d1cc6f30682
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-02-10 18:12:22 +0000

    Created basic controller file for main views.

---
+++ bbc/controllers/main_controller.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+get "/" do
+    'Hello world!'
+end
+
+get "/user" do
+    'Hello world!'
+end
+
+get "/administrator" do
+    'Hello world!'
+end
+
+get "/staff" do
+    'Hello world!'
+end
