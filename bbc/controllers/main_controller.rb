bca22055ee965e20a122738e067c320b12ae4210
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-02-10 18:43:37 +0000

    Created controllers for all user paths.

--- bbc/controllers/main_controller.rb
+++ bbc/controllers/main_controller.rb
@@ -3,15 +3,3 @@ require 'sinatra'
 get "/" do
     'Hello world!'
 end
-
-get "/user" do
-    'Hello world!'
-end
-
-get "/administrator" do
-    'Hello world!'
-end
-
-get "/staff" do
-    'Hello world!'
-end
