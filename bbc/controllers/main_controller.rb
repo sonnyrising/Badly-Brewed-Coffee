905a88c37fe8ebd66235770a0955e267a9b06153
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-02-14 00:18:02 +0000

    Changed page titles and added yield block to login page.

--- bbc/controllers/main_controller.rb
+++ bbc/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'sqlite3'
+require 'json'
+
+#DB = SQLite3::Database.new '../db/usersdatabase.db'
+#DB.results_as_hash = true
 
 get "/" do
   redirect "/public"
@@ -12,9 +17,7 @@ post "/login" do
     username = params[:uname]
     password = params[:pword]
 
-    if username == "test" && password == "test"
-      "Logged in successfully!"
-    else
-      redirect "/login"
-    end
+    #user = DB.get_first_row("SELECT * FROM Users WHERE username = ?", [username])
+
+    #puts "First result: #{user}"
 end
