123f1f1d1f472ea4c1a3c1224a3f51c0dc9f0116
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-03-07 16:32:12 +0000

    Created and populated database with test data.

--- bbc/db/data.sql
+++ bbc/db/data.sql
@@ -1,1 +1,11 @@
-INSERT INTO Users VALUE (1, 'UsernameHere', 'PasswordHere', 'EmailHere', 5);
+INSERT INTO Users VALUES (1, 'John', 'PasswordHere', 'johnpork@gmail.com', 5);
+INSERT INTO Users VALUES (2, 'Bill', 'PasswordHere', 'billboy@yahoo.com', 2);
+INSERT INTO Users VALUES (3, 'Bob', 'PasswordHere', 'bobby@hotmail.co.uk', 7);
+INSERT INTO Users VALUES (4, 'Ross', 'PasswordHere', 'rossbob@outlook.com', 6);
+INSERT INTO Users VALUES (5, 'Steve', 'PasswordHere', 'steveneven@gmail.com', 4);
+
+INSERT INTO Products VALUES (1, 'JorgeBean', 12.99, 10);
+INSERT INTO Products VALUES (2, 'Ethiopian Yirgacheffe', 4.99, 256);
+INSERT INTO Products VALUES (3, 'Colombian Supremo', 3.99, 300);
+INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 7.99, 112);
+INSERT INTO Products VALUES (5, 'Kenyan AA', 10.99, 400);
