5f9c242de2dde37db89dd0266e8ce57f3e341461
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-03-09 15:42:00 +0000

    Fixed some heading links.

--- bbc/controllers/main_controller.rb
+++ bbc/controllers/main_controller.rb
@@ -90,7 +90,7 @@ post "/register" do
 end
 
 get "/logout" do
-  redirect "/landingpage"
+  redirect "/login"
 end
 
 #----------------------------------- USER ROUTES ---------------------------------
