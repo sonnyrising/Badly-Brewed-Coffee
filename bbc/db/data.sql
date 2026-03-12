54bab78a96619a043bd16b1d4a96842446a3b1e6
Sonny Rising <strising1@sheffield.ac.uk>
2026-03-12 15:22:19 +0000

    Changed databases to include image type

--- bbc/db/data.sql
+++ bbc/db/data.sql
@@ -4,9 +4,9 @@ INSERT INTO Users VALUES (3, 'Bob', 'PasswordHere', 'bobby@hotmail.co.uk', 7, 1)
 INSERT INTO Users VALUES (4, 'Ross', 'PasswordHere', 'rossbob@outlook.com', 6, 1);
 INSERT INTO Users VALUES (5, 'Steve', 'PasswordHere', 'steveneven@gmail.com', 4, 1);
 
-INSERT INTO Products VALUES (1, 'JorgeBean','jorge_bean', 'The rich mexican Jorge bean',12.99, 10);
-INSERT INTO Products VALUES (2, 'Ethiopian Yirgacheffe', 'Ethiopian_Yirgacheffe','Filled with  joy and flavour',4.99, 256);
-INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Colombian_Supremo','Authentic colombian taste',3.99, 300);
-INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Guatemalan_Antigua','Makes you taste nostalgia',7.99, 112);
-INSERT INTO Products VALUES (5, 'Kenyan AA','Kenyan_AA', 'Strong start to your morning',10.99, 400);
-INSERT INTO Products VALUES (6, 'Amazonian Blend','Amazonian_blend', 'Feel the river flow',6.59, 398);
+INSERT INTO Products VALUES (1, 'JorgeBean','jorge_bean.jpeg', 'The rich mexican Jorge bean',12.99, 10);
+INSERT INTO Products VALUES (2, 'Ethiopian Yirgacheffe', 'Ethiopian_Yirgacheffe.webp','Filled with  joy and flavour',4.99, 256);
+INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Colombian_Supremo.webp','Authentic colombian taste',3.99, 300);
+INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Guatemalan_Antigua.webp','Makes you taste nostalgia',7.99, 112);
+INSERT INTO Products VALUES (5, 'Kenyan AA','Kenyan_AA.jpg', 'Strong start to your morning',10.99, 400);
+INSERT INTO Products VALUES (6, 'Amazonian Blend','Amazonian_blend.jpeg', 'Feel the river flow',6.59, 398);
