8363dfbdd10da2283567bdc43a74a29a4c5a9929
Jorge Garcia Alcantara <jcgarciaalcantara1@sheffield.ac.uk>
2026-03-12 12:57:33 +0000

    Added pictures for the beans and updated information in the beans table

--- bbc/db/data.sql
+++ bbc/db/data.sql
@@ -4,8 +4,9 @@ INSERT INTO Users VALUES (3, 'Bob', 'PasswordHere', 'bobby@hotmail.co.uk', 7, 1)
 INSERT INTO Users VALUES (4, 'Ross', 'PasswordHere', 'rossbob@outlook.com', 6, 1);
 INSERT INTO Users VALUES (5, 'Steve', 'PasswordHere', 'steveneven@gmail.com', 4, 1);
 
-INSERT INTO Products VALUES (1, 'JorgeBean','Temp text', 'The rich mexican Jorge bean',12.99, 10);
-INSERT INTO Products VALUES (2, 'Ethiopian Yirgacheffe', 'Temp text','Filled with  joy and flavour',4.99, 256);
-INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Temp text','Authentic colombian taste',3.99, 300);
-INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Temp text','Makes you taste nostalgia',7.99, 112);
-INSERT INTO Products VALUES (5, 'Kenyan AA','Temp text', 'Strong start to your morning',10.99, 400);
+INSERT INTO Products VALUES (1, 'JorgeBean','jorge_bean', 'The rich mexican Jorge bean',12.99, 10);
+INSERT INTO Products VALUES (2, 'Ethiopian Yirgacheffe', 'Ethiopian_Yirgacheffe','Filled with  joy and flavour',4.99, 256);
+INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Colombian_Supremo','Authentic colombian taste',3.99, 300);
+INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Guatemalan_Antigua','Makes you taste nostalgia',7.99, 112);
+INSERT INTO Products VALUES (5, 'Kenyan AA','Kenyan_AA', 'Strong start to your morning',10.99, 400);
+INSERT INTO Products VALUES (6, 'Amazonian Blend','Amazonian_blend', 'Feel the river flow',6.59, 398);
