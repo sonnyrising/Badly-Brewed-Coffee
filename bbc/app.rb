c8d32681d65a1b0b58f4f12935d3225dcca83b16
Jorge Garcia Alcantara <jcgarciaalcantara1@sheffield.ac.uk>
2026-03-15 13:15:45 +0000

    added a secret key to the session so we can access session details and made it so products can be added to the users basket inside the DB with their ID

--- bbc/app.rb
+++ bbc/app.rb
@@ -9,3 +9,4 @@ require_all "models"
 set :views, File.join(__dir__, "views")
 
 enable :sessions
+set :session_secret, "bbc_super_secret_key_to_make_it able_to_access_session_details_that_needs_to_be_64_characters_long"
