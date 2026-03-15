a56d969777af978bcbaff0c6070104f712ef1155
Jorge Garcia Alcantara <jcgarciaalcantara1@sheffield.ac.uk>
2026-03-15 14:41:11 +0000

    working and struggling to display added items in basket

--- bbc/models/Basket.rb
+++ bbc/models/Basket.rb
@@ -6,5 +6,41 @@ class Basket < Sequel::Model(:Basket)
     self.Quantity = params.fetch("quantity","").strip
     self.OrderStatus = "null"
   end
+
+  def self.numOfProductsInBasket(userId)
+    count = 0
+    Basket.each do |product|
+      count += product.Quantity if product.UserId == userId
+    end
+
+    return count
+  end
+
+  def self.getProductNamesForUser(userId)
+    names = Array.new
+    Basket.each do |b_product|
+      if b_product.UserId == userId
+        Products.each do |product|
+          if b_product.ProductId == product.ProductId 
+            if names.empty?
+              names << product.ProductName
+            else
+              names.each do |n|
+                if n == product.ProductName
+                  break
+                else
+                  names << product.ProductName
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    return names
+  end
+
+
   
 end
