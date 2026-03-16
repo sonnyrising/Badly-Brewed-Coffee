fc9e83b3aad4963d248d57d6203127544bc513e6
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-03-16 16:17:32 +0000

    Fixed some merge conflict stuff

--- bbc/models/staff.rb
+++ bbc/models/staff.rb
@@ -8,14 +8,7 @@ class Staff < Sequel::Model
     return Staff.where(StaffId: staffId).get(:StaffUsername)
   end
 
-<<<<<<< HEAD
-  def self.GetEmail(staffId)
-    return Staff.where(StaffId: staffId).get(:StaffEmail)
-  end
-end    
-=======
     def self.GetEmail(staffId)
       return Staff.where(StaffId: staffId).get(:StaffEmail)
     end
 end
->>>>>>> ebf24c0 (fixed staff database issue)
