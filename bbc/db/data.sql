b8a4c7d6e96ce731878e12b512209a93295db09b
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-03-16 20:58:56 +0000

    Finished account transaction history admin view.

--- bbc/db/data.sql
+++ bbc/db/data.sql
@@ -11,3 +11,5 @@ INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Colombian_Supremo.webp','A
 INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Guatemalan_Antigua.webp','Makes you taste nostalgia',7.99, 112);
 INSERT INTO Products VALUES (5, 'Kenyan AA','Kenyan_AA.jpg', 'Strong start to your morning',10.99, 400);
 INSERT INTO Products VALUES (6, 'Amazonian Blend','Amazonian_blend.jpeg', 'Feel the river flow',6.59, 398);
+
+INSERT INTO Transactions VALUES (1, 1, 1, 10, 12.99, "Complete", 16032026);
