c832b002553998d0973e5c3f3d5f1604bbd269ca
Sam Perkins <SLPerkins1@sheffield.ac.uk>
2026-03-16 15:55:23 +0000

    fixed staff schema

--- bbc/models/staff.rb
+++ bbc/models/staff.rb
@@ -1,3 +1,5 @@
+class Staff < Sequel::Model
+    
     def self.GetStaffID(username)
         staffId = Staff.where(Username: "#{username}").get(:StaffId)
         return staffId
@@ -6,3 +8,9 @@
     def self.GetUsername(staffId)
       return Staff.where(StaffId: staffId).get(:StaffUsername)
     end
+
+    def self.GetEmail(staffId)
+      return Staff.where(StaffId: staffId).get(:StaffEmail)
+    end
+  
+    
