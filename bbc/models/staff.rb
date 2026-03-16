65f60a901098b1077d6a8d00dfa6765c255e7b80
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-03-16 16:00:14 +0000

    Changed staff model to work lol

--- bbc/models/staff.rb
+++ bbc/models/staff.rb
@@ -1,16 +1,14 @@
-class Staff < Sequel::Model
-    
-    def self.GetStaffID(username)
-        staffId = Staff.where(Username: "#{username}").get(:StaffId)
-        return staffId
-    end
+class Staff < Sequel::Model  
+  def self.GetStaffID(username)
+      staffId = Staff.where(Username: "#{username}").get(:StaffId)
+      return staffId
+  end
 
-    def self.GetUsername(staffId)
-      return Staff.where(StaffId: staffId).get(:StaffUsername)
-    end
+  def self.GetUsername(staffId)
+    return Staff.where(StaffId: staffId).get(:StaffUsername)
+  end
 
-    def self.GetEmail(staffId)
-      return Staff.where(StaffId: staffId).get(:StaffEmail)
-    end
-  
-    
+  def self.GetEmail(staffId)
+    return Staff.where(StaffId: staffId).get(:StaffEmail)
+  end
+end    
