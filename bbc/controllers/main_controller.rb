0147454b9d8f7aad76bb1ad397a4a73371749e20
Sonny Rising <strising1@sheffield.ac.uk>
2026-04-21 12:30:28 +0100

    Refactoring: Reduced length of refunddetails method

--- bbc/controllers/main_controller.rb
+++ bbc/controllers/main_controller.rb
@@ -521,19 +521,13 @@ get "/manager/refunds" do
 end
 
 get "/manager/refunddetails" do
-  transaction_id = params[:transaction_id]
-  user_id = Transactions.where(TransactionId: transaction_id).get(:UserId)
-  total_cost = Transactions.where(TransactionId: transaction_id).get(:TotalCost)
-  transaction_date = format_date(Transactions.where(TransactionId: transaction_id).get(:TransactionDate))
-  status = Transactions.where(TransactionId: transaction_id).get(:Status)
-  refund_reason = Feedbacks.where(TransactionId: transaction_id).get(:RefundReason)
   @refund_info = {
-    transaction_id: transaction_id,
-    user_id: user_id,
-    total_cost: total_cost,
-    transaction_date: transaction_date,
-    status: status,
-    refund_reason: refund_reason
+    transaction_id: params[:transaction_id],
+    user_id: Transactions.where(TransactionId: transaction_id).get(:UserId),
+    total_cost: Transactions.where(TransactionId: transaction_id).get(:TotalCost),
+    transaction_date: format_date(Transactions.where(TransactionId: transaction_id).get(:TransactionDate)),
+    status: Transactions.where(TransactionId: transaction_id).get(:Status),
+    refund_reason: Feedbacks.where(TransactionId: transaction_id).get(:RefundReason)
   }
 
   erb :"manager/refunddetails"
