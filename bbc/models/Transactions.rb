fcdd39b538d8d02354a0e61c38fefff2bd5e6ee7
Jorge Garcia Alcantara <jcgarciaalcantara1@sheffield.ac.uk>
2026-04-28 17:56:23 +0000

    Implemented address to transaction creation

--- bbc/models/Transactions.rb
+++ bbc/models/Transactions.rb
@@ -2,6 +2,7 @@ class Transactions < Sequel::Model
   def load(params)
     self.UserId = params.fetch("userId", "")
     self.Quantity = params.fetch("quantity", "")
+    self>Address = params.fetch("address", "")
     self.TransactionDate = 0
   end
 end
