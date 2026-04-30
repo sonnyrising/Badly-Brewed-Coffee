18af660fc77d5331fde1629fba754d71d92d612f
Sonny Rising <strising1@sheffield.ac.uk>
2026-04-30 14:05:47 +0100

    Refactoring: Removed duplicate code and views for staff and manager

--- bbc/controllers/main_controller.rb
+++ bbc/controllers/main_controller.rb
@@ -29,3 +29,107 @@ get "/" do
   session.clear
   redirect "/landingpage"
 end
+
+#------------------------------ Manager and Staff Shared Functionality -------------------------------
+get "/orders" do
+  @orders = Transactions.all
+  erb :"orders"
+end
+
+
+get "/refunds" do
+  @refunds = Transactions.where(refundRequested: true)
+
+  if params[:message] == "accept"
+    @alert_message = "Refund has been accepted."
+    Transactions.where(TransactionId: params[:refund_id]).update(refundRequested: false)
+  elsif params[:message] == "decline"
+    @alert_message = "Refund has been declined."
+    Transactions.where(TransactionId: params[:refund_id]).update(refundRequested: false)
+  end
+
+  erb :"refunds"
+end
+
+get "/refunddetails" do
+  t_id = params[:transaction_id]
+
+  @refund_info = {
+    transaction_id: t_id,
+    user_id: Transactions.where(TransactionId: t_id).get(:UserId),
+    total_cost: Transactions.where(TransactionId: t_id).get(:TotalCost),
+    transaction_date: format_date(Transactions.where(TransactionId: t_id).get(:TransactionDate)),
+    status: Transactions.where(TransactionId: t_id).get(:Status),
+    refund_reason: Feedbacks.where(TransactionId: t_id).get(:RefundReason)
+  }
+
+  erb :"refunddetails"
+end
+
+post "/updatestatus" do
+  transaction_id = params[:transaction_id]
+  new_status = params[:status]
+
+  Transactions.where(TransactionId: transaction_id).update(Status: new_status)
+
+  redirect "/orders"
+end
+
+get "/managestock" do
+  @products = Products.all
+
+  @alert_message = case params[:error]
+                   when "invalid_name"  then "Please enter a valid product name."
+                   when "invalid_stock" then "Please enter a valid stock quantity."
+                   when "invalid_price" then "Please enter a valid price."
+                   when "invalid_image" then "Please enter a valid image URL."
+                   when "invalid_description" then "Please enter a valid description."
+                   when "invalid_origin" then "Please enter a valid origin."
+                   when "invalid_roast" then "Please enter a valid roast."
+                   end
+  erb :"managestock"
+end
+
+# Updates the product table based on user inputs
+post '/updatestock' do
+  values_hash = {
+    product: Products[params['product_id']],
+    # Sanitise the input before updating the database
+    name: sanitise_string(params['product_name']),
+    stock: sanitise_int(params['product_stock']),
+    price: sanitise_price(params['product_price']),
+    image: sanitise_string(params['product_image']),
+    description: sanitise_string(params['product_description']),
+    origin: sanitise_string(params['product_origin']),
+    roast: sanitise_string(params['product_roast'])
+  }
+  update_product_values(values_hash)
+
+  redirect 'managestock'
+end
+
+post "/deleteproduct" do
+  product_id = params[:product_id]
+  Products.where(ProductId: product_id).delete
+  redirect "managestock"
+end
+
+get "/addproduct" do
+  erb :"addproduct"
+end
+
+post "/addproduct" do
+  highest_id = Products.max(:ProductId)
+  @products = Products.all
+  Products.insert(
+    ProductId: highest_id + 1,
+    ProductName: sanitise_string(params['product_name']),
+    StockQuantity: sanitise_int(params['product_stock']),
+    Price: sanitise_price(params['product_price']),
+    ProductImage: sanitise_string(params['product_image']),
+    ProductDescription: sanitise_string(params['product_description']),
+    Origin: sanitise_string(params['product_origin']),
+    Roast: sanitise_string(params['product_roast'])
+  )
+  redirect "/managestock"
+end
