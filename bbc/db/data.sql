3de3887ae5a6106db7aec58b8e392942c87c5792
Steffan Falkenberg <sjfalkenberg1@sheffield.ac.uk>
2026-05-01 14:44:42 +0000

    Fixed login page button formatting. Fixed schema file and data file so now database creation works again.

--- bbc/db/data.sql
+++ bbc/db/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Users VALUES (1, 'Guest', null,null, null,null, null, null, null, null);
+INSERT INTO Users VALUES (1, 'Guest', null, null, null,null, null, null, null, null, 0);
 INSERT INTO Users VALUES (2, 'Bill', 'PasswordHere', 'billboy@yahoo.com', 2, 1, 0, 0, 0, 0, 0);
 INSERT INTO Users VALUES (3, 'Bob', 'PasswordHere', 'bobby@hotmail.co.uk', 7, 1, 0, 0, 0, 2, 0);
 INSERT INTO Users VALUES (4, 'Ross', 'PasswordHere', 'rossbob@outlook.com', 6, 1, 0, 0, 0, 4, 0);
@@ -11,12 +11,12 @@ INSERT INTO Products VALUES (3, 'Colombian Supremo', 'Colombian_Supremo.webp','A
 INSERT INTO Products VALUES (4, 'Guatemalan Antigua', 'Guatemalan_Antigua.webp','Makes you taste nostalgia', 'Light', 'Guatamala', 7.99, 112);
 INSERT INTO Products VALUES (5, 'Kenyan AA','Kenyan_AA.jpg', 'Strong start to your morning', 'Dark', 'Kenya', 10.99, 400);
 INSERT INTO Products VALUES (6, 'Amazonian Blend','Amazonian_blend.jpeg', 'Feel the river flow', 'Medium', 'Brazil', 6.59, 398);
-INSERT INTO Products VALUES (7, 'Roniccino', 'roniccino.jpg', '', 'Dark', 'Guatemala' 2.99, 100000);
-INSERT INTO Products VALUES(8, 'Azmocha', 'Azmocha.png', '', 'Light', 'Colombia' 2.99, 100000);
-INSERT INTO Products VALUES(9, 'Steffspresso', 'Steffspresso.png', '', 'Dark', 'Ethiopia' 2.29, 100000);
-INSERT INTO Products VALUES(10, 'Sonnycaf', 'SonnyCaf.png', '', 'Medium', 'Kenya' 1.99, 100000);
-INSERT INTO Products VALUES (11, 'Jortado', 'Jortado.png', '', 'Dark', 'Mexico' 3.29, 100000);
-INSERT INTO Products VALUES (12, 'Samacchiato', 'Samacchiarto.png', '', 'Light', 'Brazil' 1.99, 100000);
+INSERT INTO Products VALUES (7, 'Roniccino', 'roniccino.jpg', '', 'Dark', 'Guatemala', 2.99, 100000);
+INSERT INTO Products VALUES(8, 'Azmocha', 'Azmocha.png', '', 'Light', 'Colombia', 2.99, 100000);
+INSERT INTO Products VALUES(9, 'Steffspresso', 'Steffspresso.png', '', 'Dark', 'Ethiopia', 2.29, 100000);
+INSERT INTO Products VALUES(10, 'Sonnycaf', 'SonnyCaf.png', '', 'Medium', 'Kenya', 1.99, 100000);
+INSERT INTO Products VALUES (11, 'Jortado', 'Jortado.png', '', 'Dark', 'Mexico', 3.29, 100000);
+INSERT INTO Products VALUES (12, 'Samacchiato', 'Samacchiarto.png', '', 'Light', 'Brazil', 1.99, 100000);
 
 INSERT INTO Transactions VALUES (1,1, 12.99, "SteffanRoad",24032026, 'Pending', false, null);
 INSERT INTO Transactions VALUES (2,2, 24.54,"Sam grove", 24032026, 'Pending', true, null);
--- bbc/db/schema.sql
+++ bbc/db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE Users(
   Warning INTEGER,
   Suspended INTEGER,
   FreeCoffeesRedeemed INTEGER,
-  LoyaltyDiscount
+  LoyaltyDiscount FLOAT
 );
 
 CREATE TABLE Transactions(
