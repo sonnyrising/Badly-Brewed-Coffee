9912361e27aea677bb361a3ed8e2af6d00d83962
Safwan Maz Mirza <masmazmirza1@sheffield.ac.uk>
2026-05-10 14:13:36 +0000

    Added rspec testing for account simulation

--- bbc/spec/controllers/admin_spec.rb
+++ bbc/spec/controllers/admin_spec.rb
@@ -112,4 +112,30 @@ RSpec.describe "Feedback Listing Tests" do
       expect(Feedbacks.where(FeedbackId: 1).count).to eq(0)
     end
   end
+
+# =============================================================================
+# Simulating account view
+# =============================================================================
+RSpec.describe "Simulating account view" do
+
+  let(:admin_session)   { { 'rack.session' => { user_id: 1, uname: 'admin' } } }
+
+  accounts_viewing_routes = [
+    "/admin/views/manager",
+    "/admin/views/barista",
+    "/admin/views/user"
+  ]
+
+  accounts_viewing_routes.each do |route|
+    describe "POST #{route}" do  
+
+      context "viewing accounts" do
+        it "viewing as other roles' (barista, manager, user)" do
+          post route, {}, admin_session
+          expect(last_response.status).to eq(302)
+          expect(last_response.location).not_to eq("http://example.org/")
+        end
+      end
+    end
+  end
 end
