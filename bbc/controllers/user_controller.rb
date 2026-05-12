85bc87759f7c6080cd8da0b928fa6f6b0a8e2912
Jorge Garcia Alcantara <jcgarciaalcantara1@sheffield.ac.uk>
2026-05-12 20:40:09 +0000

    Finally implemented the ability to use the basket after checkout

--- bbc/controllers/user_controller.rb
+++ bbc/controllers/user_controller.rb
@@ -52,7 +52,15 @@ post "/user/shop" do
   product_exists = @basket.productCheck(params)
   item_bought = @basket.bought(params)
 
-  if exists.nil? || user_exists.nil? || product_exists.nil? || item_bought.nil?
+  if !item_bought.nil?
+    latest_product = @basket.checkLatest(params)
+    if latest_product.nil?
+      @basket.addToBasket(params)
+      @basket.save_changes
+    else 
+      @basket.updateQuantity(params)
+    end
+  elsif exists.nil? || user_exists.nil? || product_exists.nil?
     @basket.addToBasket(params)
     @basket.save_changes
   else
